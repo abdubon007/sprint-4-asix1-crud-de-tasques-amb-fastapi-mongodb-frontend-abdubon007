fe702ed496d58421ac81388d4d129046bb1a9544
abdubon007 <abdullahmuhammad@iesmontsia.org>
2026-04-20 00:09:21 +0200

    javascript.js

--- frontend/javascript.js
+++ frontend/javascript.js
@@ -1,80 +1,111 @@
-//===Any global variables whose scope will need to be across the entire file...
-var currentId;
-
-//===Actually, I am kind of a big fan of defining a Global object, or something
-//	 similar to store all of my page-level variables to. Something like:
-var Global = {
-	currentId: undefined,
-	action: 'create',
-	user: {
-		userName: 'Bob',
-		email: 'bgibilaro@valexander.com',
-		extension: '2470'
-	}
-};
-
-// this allows me to get those values anywhere in the page with
-// something like Global.currentId or Global.user.userName. I can also set it by saying
-// Global.currentId = 2. I can even add to it on-the-fly by saying
-// Global.newVariable = 'something' which is now available over the
-// entirety of that page....
-
-//===My document.ready() handler...
-$(document).ready(function(){
-
-	//=== do some code stuff...
-
-	//===finally, bind my events...
-	bindEvents();
-});
-
-//===This function handles event binding for anything on the page....
-function bindEvents(){
-	// So, something simply like binding to a static anchor tag...
-	$('#aSomeLink').on('click', function(event){
-
-		// do some cool code stuff...
-		// Mr. Wizard Time: try putting a break point someplace in here and
-		//	then investigate the event argument. All sorts of cool stuff can
-		//	come from there. In fact, what if I wanted to assign the link that
-		//	was clicked to a local variable?
-
-		// I could do this...
-		$a = $(this);	// note, prefixing the variable with bling ($) is just a 
-						// nice way for us to know that it is a jQuery object...
-
-		// Or, I could do this...
-		$a = $(event.target);
-
-		// I could also get the id of the link like so:
-		var id = event.target.id;
-
-		// not a big difference, but it is nice to use native JavaScript when you can. 
-	});
-
-	// Hey, but I can also do something cooler with the on method. What if I have a table
-	//	full of documents on the page with the option to edit, delete, etc. each of the table
-	//	rows. I can handle this in one nice bind using on. for the sake of this example, let's
-	//	assume I gave the "delete" link an attribute of rel="delete" and the "edit" link an 
-	//	attribute of rel="edit", I could do the following:
-	$('#myTable').on('click', 'a[rel=delete],a[rel=edit]', function(event){
-		$a = $(event.target);
-
-		switch($a.attr('rel')){
-			case 'edit':
-
-				// do some stuff or call a function...
-				
-				break;
-			case 'delete':
-				// do some stuff or call a function...
-
-				break;
-		}
-	});
-
-	// the above allows you to setup your bindings one time, when the page loads and then forget about
-	//	it. It will apply those bindings any time a new row is added to #myTable, automagically...
+const API = "http://127.0.0.1:8000";
+ 
+// CREATE - POST /books/
+async function createBook() {
+  const titol = document.getElementById("titol").value;
+  const autor = document.getElementById("autor").value;
+  const estat = document.getElementById("estat").value;
+  const valoracio = parseInt(document.getElementById("valoracio").value);
+  const categoria = document.getElementById("categoria").value;
+  const persona = document.getElementById("persona").value;
+ 
+  if (!titol || !autor || !persona) {
+    document.getElementById("create-msg").innerText = "Error: títol, autor i persona són obligatoris.";
+    return;
+  }
+ 
+  const res = await fetch(`${API}/books/`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ titol, autor, estat, valoracio, categoria, persona })
+  });
+ 
+  const data = await res.json();
+  document.getElementById("create-msg").innerText = "Llibre creat! ID: " + data._id;
+  loadBooks();
 }
-
-//===Then everything below this is all of the other declared functions for my page...
+ 
+// READ ALL - GET /books/
+async function loadBooks() {
+  const res = await fetch(`${API}/books/`);
+  const data = await res.json();
+  const container = document.getElementById("book-list");
+ 
+  if (data.books.length === 0) {
+    container.innerHTML = "<p>No hi ha llibres.</p>";
+    return;
+  }
+ 
+  container.innerHTML = data.books.map(b =>
+    `<div>
+      <b>${b.titol}</b> - ${b.autor} | Estat: ${b.estat} | Valoració: ${b.valoracio} | Categoria: ${b.categoria} | Persona: ${b.persona}
+      <br><small>ID: ${b._id}</small>
+    </div>`
+  ).join("");
+}
+ 
+// READ ONE - GET /books/{id}
+async function getBookById() {
+  const id = document.getElementById("search-id").value;
+  const res = await fetch(`${API}/books/${id}`);
+ 
+  if (res.status === 404) {
+    document.getElementById("book-detail").innerText = "Llibre no trobat.";
+    return;
+  }
+ 
+  const b = await res.json();
+  document.getElementById("book-detail").innerHTML =
+    `<b>${b.titol}</b> - ${b.autor}<br>
+     Estat: ${b.estat}<br>
+     Valoració: ${b.valoracio}<br>
+     Categoria: ${b.categoria}<br>
+     Persona: ${b.persona}`;
+}
+ 
+// UPDATE - PUT /books/{id}
+async function updateBook() {
+  const id = document.getElementById("update-id").value;
+  if (!id) {
+    document.getElementById("update-msg").innerText = "Cal introduir un ID.";
+    return;
+  }
+ 
+  const payload = {};
+  const estat = document.getElementById("update-estat").value;
+  const valoracio = document.getElementById("update-valoracio").value;
+ 
+  if (estat) payload.estat = estat;
+  if (valoracio) payload.valoracio = parseInt(valoracio);
+ 
+  const res = await fetch(`${API}/books/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload)
+  });
+ 
+  if (res.status === 404) {
+    document.getElementById("update-msg").innerText = "Llibre no trobat.";
+    return;
+  }
+ 
+  const data = await res.json();
+  document.getElementById("update-msg").innerText = "Actualitzat! Estat: " + data.estat;
+  loadBooks();
+}
+ 
+// DELETE - DELETE /books/{id}
+async function deleteBook() {
+  const id = document.getElementById("delete-id").value;
+  const res = await fetch(`${API}/books/${id}`, { method: "DELETE" });
+ 
+  if (res.status === 204) {
+    document.getElementById("delete-msg").innerText = "Llibre eliminat correctament.";
+    loadBooks();
+  } else {
+    document.getElementById("delete-msg").innerText = "Error: Llibre no trobat.";
+  }
+}
+ 
+// Carregar llibres al iniciar la pàgina
+window.onload = loadBooks;
